cdcc99c3edd7579961a48a2443ba406872abe7eb
Zijian Zhang <futrime@outlook.com>
2023-10-12 07:45:18 +0800

    Update script

--- release.sh
+++ release.sh
@@ -1,36 +1,39 @@
 #! /usr/bin/env bash
 
-# $1 should be in form x.y.z.w
-if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Invalid version number: $BDS_VERSION"
-    exit 1
-fi
-
-# Set $BDS_VERSION and $TOOTH_VERSION
-BDS_VERSION=$1 # x.y.z.w
-TOOTH_VERSION=$(echo $BDS_VERSION | cut -d. -f1-3) # x.y.z
-
-# Read tooth.template.json into memory
 TOOTH_TEMPLATE=$(cat tooth.template.json)
+VERSIONS=$(cat versions.txt)
 
-# Replace every <BDS_VERSION> with $BDS_VERSION and every <TOOTH_VERSION> with $TOOTH_VERSION
-TOOTH_TEMPLATE=${TOOTH_TEMPLATE//<BDS_VERSION>/$BDS_VERSION}
-TOOTH_TEMPLATE=${TOOTH_TEMPLATE//<TOOTH_VERSION>/$TOOTH_VERSION}
+# For each line of versions.txt, run release()
+for VERSION in $VERSIONS; do
+    release $VERSION
+done
 
-# Check if release/$TOOTH_VERSION already exists
-if git rev-parse --verify release/$TOOTH_VERSION >/dev/null 2>&1; then
-    echo "release/$TOOTH_VERSION already exists"
-    exit 1
-fi
+# Usage release <BDS_VERSION>
+release() {
+    # Set $BDS_VERSION and $TOOTH_VERSION
+    BDS_VERSION=$1 # x.y.z.w
+    TOOTH_VERSION=$(echo $BDS_VERSION | cut -d. -f1-3) # x.y.z
 
-# Create release/$TOOTH_VERSION
-git checkout -b release/$TOOTH_VERSION --force $(git rev-list --max-parents=0 HEAD)
+    # If tag v$TOOTH_VERSION already exists, exit
+    if git rev-parse v$TOOTH_VERSION >/dev/null 2>&1; then
+        echo "Tag v$TOOTH_VERSION already exists"
+        return
+    fi
 
-# Write tooth.json
-echo "$TOOTH_TEMPLATE" > tooth.json
+    # Replace every <BDS_VERSION> with $BDS_VERSION and every <TOOTH_VERSION> with $TOOTH_VERSION
+    TOOTH_CONTENT=$TOOTH_TEMPLATE
+    TOOTH_CONTENT=${TOOTH_CONTENT//<BDS_VERSION>/$BDS_VERSION}
+    TOOTH_CONTENT=${TOOTH_CONTENT//<TOOTH_VERSION>/$TOOTH_VERSION}
 
-# Commit and push
-git add tooth.json
-git commit -m "Release $TOOTH_VERSION"
-git tag v$TOOTH_VERSION
-git push origin v$TOOTH_VERSION
+    # Create release
+    git checkout -b release --force $(git rev-list --max-parents=0 HEAD)
+
+    # Write tooth.json
+    echo "$TOOTH_CONTENT" > tooth.json
+
+    # Commit and push
+    git add tooth.json
+    git commit -m "Release $TOOTH_VERSION"
+    git tag v$TOOTH_VERSION
+    git push origin v$TOOTH_VERSION
+}
