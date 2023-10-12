47a86800379f211fdd02f631240de82419d3dd48
Futrime <futrime@outlook.com>
2023-10-12 10:27:39 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,13 +1,24 @@
 #! /usr/bin/env bash
 
-# Usage release <BDS_VERSION>
-release() {
+TOOTH_TEMPLATE=$(cat tooth.template.json)
+VERSIONS=$(cat versions.txt)
+
+# For each line of versions.txt, run release()
+for VERSION in $VERSIONS; do
     git reset --hard
     git checkout -b release $(git rev-list --max-parents=0 HEAD)
 
     # Set $BDS_VERSION and $TOOTH_VERSION
-    BDS_VERSION=$1 # x.y.z.w
+    BDS_VERSION=$VERSION # x.y.z.w
     TOOTH_VERSION=$(echo $BDS_VERSION | cut -d. -f1-3) # x.y.z
+
+    # If $TOOTH_VERSION is already tagged, skip
+    if git rev-parse v$TOOTH_VERSION >/dev/null 2>&1; then
+        echo "Skipping $TOOTH_VERSION"
+        git checkout main
+        git branch -D release
+        continue
+    fi
 
     # Replace every <BDS_VERSION> with $BDS_VERSION and every <TOOTH_VERSION> with $TOOTH_VERSION
     TOOTH_CONTENT=$TOOTH_TEMPLATE
@@ -25,18 +36,6 @@ release() {
     # Clean up
     git checkout main
     git branch -D release
-}
-
-TOOTH_TEMPLATE=$(cat tooth.template.json)
-VERSIONS=$(cat versions.txt)
-
-# Remove all tags
-git tag -l | xargs git tag -d
-git push --tags --prune origin
-
-# For each line of versions.txt, run release()
-for VERSION in $VERSIONS; do
-    release $VERSION
 done
 
 git push --tags origin
